9d46f39d13076a4c50ec11b7756d2c157bb1ab4b
Nikunj2022 <nikunj.rawat@flybots.co.in>
2026-04-13 00:33:55 +0530

    Update config.h

--- firmware/config.h
+++ firmware/config.h
@@ -1,16 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define PIN_MOTOR_1 4   // Front Right
-#define PIN_MOTOR_2 33  // Front Left
-#define PIN_MOTOR_3 32  // Rear Left
-#define PIN_MOTOR_4 25  // Rear Right
+// Motors
+#define PIN_MOTOR_1 4
+#define PIN_MOTOR_2 33
+#define PIN_MOTOR_3 32
+#define PIN_MOTOR_4 25
 
+// WiFi
 #define WIFI_SSID "ESP32_Drone"
 #define WIFI_PASS "123456789"
 #define UDP_PORT 8888
 
-#define PID_PITCH_KP 0.8f  // Lowered for stability
+// Stability PID
+#define PID_PITCH_KP 0.8f
 #define PID_PITCH_KI 0.02f
 #define PID_PITCH_KD 0.1f
 
@@ -18,8 +21,12 @@
 #define PID_ROLL_KI 0.02f
 #define PID_ROLL_KD 0.1f
 
-#define FAILSAFE_TIMEOUT_MS 1500 // "Flywheel" safety
-#define LOOP_FREQ_HZ 100
-#define LOOP_TIME_MS (1000 / LOOP_FREQ_HZ)
+// Altitude Assist Config
+#define TARGET_HEIGHT_MM 300.0f  // Target 30cm height
+#define ALTITUDE_KP 0.4f         // How strongly it corrects height
+
+// Safety
+#define FAILSAFE_TIMEOUT_MS 1500
+#define LOOP_TIME_MS 10 // 100Hz
 
 #endif
