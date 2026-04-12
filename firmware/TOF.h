6f3aec7c61e0834d89db040fafefb9b110d6a6cb
Nikunj2022 <nikunj.rawat@flybots.co.in>
2026-04-13 00:34:27 +0530

    Update TOF.h

--- firmware/TOF.h
+++ firmware/TOF.h
@@ -1,14 +1,11 @@
 #ifndef TOF_H
 #define TOF_H
-
 #include "Adafruit_VL53L0X.h"
-
 class TOF {
 public:
-    void begin();
-    float getDistance(); // Returns distance in mm
+    bool begin();
+    float getDistance();
 private:
     Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 };
-
 #endif
